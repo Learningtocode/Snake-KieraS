ff080bc3274e6a9843bab71cd906d0ec64dfe7db
Kiera <JustForLaughts66@gmail.com>
2014-10-06 10:44:54 -0700

    Sorry accidently pushed my portfolio project saying I'm on video nine. On this project, I am on video nine.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,3 +1,23 @@
+var snake;
 
-
-
+var context; 
+var screenWidth; 
+var screenHeight; 
+ 
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen"); 
+    context = canvas.getContext("2d"); 
+     
+    screenWidth = window.innerWidth; 
+    screenHeight = window.innerHeight; 
+     
+    canvas.width = screenWidth;
+}
+ 
+function gameLoop() {
+    
+} 
+ 
+function gameDraw() {
+    
+}
