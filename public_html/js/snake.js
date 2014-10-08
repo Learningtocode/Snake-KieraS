661205bae735d780e8f768a19349a5eff1d40ca7
Kiera <JustForLaughts66@gmail.com>
2014-10-08 09:21:15 -0700

    Here you go

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,4 +1,6 @@
 var snake;
+var snakeLength; 
+var snakeSize; 
 
 var context; 
 var screenWidth; 
@@ -25,4 +27,27 @@ function gameLoop() {
 function gameDraw() {
     context.fillStyle = "rgb(0, 255, 166)"; 
     context.fillRect(0, 0, screenWidth, screenHeight);
-}
+} 
+ 
+ function snakeInitialize() {
+     snake = []; 
+     snakeLength = 5; 
+     snakeSize = 20; 
+      
+     for(var index = 0; index < snakeLength; index++) {
+         snake.push( {
+             x: index, 
+             y: 0
+         });
+     }
+ } 
+  
+ function snakeDraw() {
+     for() {
+         
+     }
+ } 
+  
+ function snakeUpdate() {
+     
+ }
